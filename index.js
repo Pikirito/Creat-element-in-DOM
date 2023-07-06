76358b9a72edb58b83c3060406ffec8c3e2f07c4
Eduardo <eduaraujp760@gmail.com>
2023-07-06 12:22:48 -0400

    Pratica de uma aula

---
+++ index.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+
+const card = document.createElement("div");
+
+card.id = "div_card";
+
+body.appendChild(card);
+
+const container_photo = document.createElement("div");
+container_photo.id = "div_photo";
+
+const container_describle = document.createElement("div");
+container_describle.id = "div_describle";
+
+card.appendChild(container_photo);
+card.appendChild(container_describle);
+
+const text = document.createElement("p");
+text.id = "p_text";
+
+text.innerText = "Funcionario em treinamento";
+
+container_describle.appendChild(text);
